2aa85faf4b8e8bb117ecdee4ef1e13a1c3bca279
ehd-studio <ericdrake@Erics-Mac-Studio.local>
2026-03-27 11:42:00 -0700

    Quartz sync: Mar 27, 2026, 11:42 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "The Digital Garden",
-    pageTitleSuffix: "a collection of thoughts/ideas built in public",
+    pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
