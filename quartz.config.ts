42088e7a11995ed83cc252a849d339a5c5a4e827
Eric Drake <ericdrake@Erics-MacBook-Pro.local>
2026-09-21 18:31:58 -0400

    Quartz sync: Sep 21, 2026, 6:31 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         title: "Doto",
-        header: "Inter",
+        header: "Host Grotesk",
         body: "Inter",
         code: "IBM Plex Mono",
       },
