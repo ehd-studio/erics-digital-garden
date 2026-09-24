d2965b8e5e4f65ded789d14af2ee95b9dafc384f
Eric Drake <ericdrake@mac.wireless.newschool.edu>
2026-09-24 15:30:09 -0400

    Quartz sync: Sep 24, 2026, 3:30 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "POINTS OF CONTACT",
-    pageTitleSuffix: "| Points of Contact",
+    pageTitleSuffix: " | Points of Contact",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
